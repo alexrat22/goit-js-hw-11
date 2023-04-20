bf5e1c4a6845c2c4c42adc72bc58053f14f0bb1f
alexrat22 <117772110+alexrat22@users.noreply.github.com>
2023-04-20 19:36:26 +0000

    Deploying to gh-pages from @ alexrat22/goit-js-hw-11@9de4aca73dc3b7d7f087ae9a006dcd92971a559b 🚀

---
+++ index.a7e6275e.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".search-form"),input:document.querySelector("input")};e.form.addEventListener("submit",(function(t){if(t.preventDefault(),console.log(e.input.value),""===e.input.value.trim())return}))}();
+//# sourceMappingURL=index.a7e6275e.js.map
---
+++ index.b877d188.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".search-form"),input:document.querySelector("input")};e.form.addEventListener("submit",(function(t){if(t.preventDefault(),console.log(e.input.value),""===e.input.value.trim())return}));
+//# sourceMappingURL=index.b877d188.js.map
