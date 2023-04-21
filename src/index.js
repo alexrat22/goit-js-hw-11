5c346177ca409b87739497839f6bc00ef183cff0
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-04-21 22:32:47 +0300

    add button load & change func submit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import './css/styles.css';
 import axios from 'axios';
-//import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
 const APIKEY = '35639448-d856c19f58ebd88d37f926e40';
+const perPage = 40;
+let page = 1;
 
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
+  loadButton: document.querySelector('.load-more'),
 };
 
 refs.form.addEventListener('submit', onSubmitButtonClick);
+refs.loadButton.addEventListener('click', onSubmitButtonClick);
 
 function onSubmitButtonClick(evt) {
   evt.preventDefault();
@@ -18,18 +22,32 @@ function onSubmitButtonClick(evt) {
   if (searchQuery.trim() === '') {
     return;
   } else {
-    getPictures(searchQuery);
+    getPictures(searchQuery).then(response => {
+      if (response.data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        const pictures = response.data.hits;
+        refs.gallery.innerHTML = pictures
+          .map(picture => createOneCardImage(picture))
+          .join('');
+        refs.loadButton.classList.remove('is-hidden');
+      }
+    });
   }
 }
 
 async function getPictures(searchQuery) {
-  const responseData = await axios.get(
-    `https://pixabay.com/api/?key=${APIKEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
-  );
-  const pictures = responseData.data.hits;
-  refs.gallery.innerHTML = pictures
-    .map(picture => createOneCardImage(picture))
-    .join('');
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${APIKEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function createOneCardImage(picture) {
